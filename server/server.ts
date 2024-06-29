63e1aada8212f1d48b966ee661ad38741ebfeb4a
nphnamm <80657434+nphnamm@users.noreply.github.com>
2024-06-29 14:52:24 +0700

    first commit

---
+++ server/server.ts
@@ -0,0 +1,10 @@
+import {app} from './app';
+require("dotenv").config();
+
+
+// todo: create server
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+    
+})
