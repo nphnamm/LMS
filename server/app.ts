70113d49bb17e2ba1519d333bfdc558d583139f5
nphnamm <80657434+nphnamm@users.noreply.github.com>
2024-09-18 23:49:31 +0700

    add test route backend

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,34 @@
+require('dotenv').config();
 import express from "express";
+
 export const app = express();
+
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
+// body parser
+app.use(express.json({limit: "50mb"}));
+
+
+// cookie parser
+app.use(cookieParser());
+
+// cors => cross origin resource sharing 
+app.use(cors({
+    origin:process.env.ORIGIN
+}))
+
+app.get("/test",(req,res,next)=>{
+    res.status(200).json({
+        success:true,
+        message:"API is working"
+    });
+
+});
+
+app.all("*",(req,res,next)=>{
+    const err = new Error(`Route ${req.originalUrl} not found`) as any;
+    err.statusCode = 404;
+    next(err);
+})
