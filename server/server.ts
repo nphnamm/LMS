1d228e62080c1b0ddf1a2f701b4ed6ae0233454c
nphnamm <80657434+nphnamm@users.noreply.github.com>
2024-09-19 11:17:53 +0700

    add cloud, redis, db

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,5 @@
 import {app} from './app';
+import connectDB from './utils/db';
 require("dotenv").config();
 
 
@@ -6,5 +7,5 @@ require("dotenv").config();
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
-    
+    connectDB();
 })
