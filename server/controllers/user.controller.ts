3ca83ff23e56aed8b443e97cdbf8cc1f9a34c4e6
nphnamm <80657434+nphnamm@users.noreply.github.com>
2024-10-13 01:02:04 +0700

    add ejs activation

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -4,7 +4,8 @@ import { CatchAsyncError } from "../middleware/cacthAsyncErrors";
 import userModel, { IUser } from "../models/user.model";
 import ErrorHandler from "../utils/ErrorHandler";
 import jwt, { Secret } from "jsonwebtoken";
-
+import ejs from "ejs"
+import path from "path"
 
 
 
@@ -40,6 +41,7 @@ export const registrationUser = CatchAsyncError(async(req:Request,res: Response,
             },
             activationCode
         }
+        const html = await ejs.renderFile(path.join(__dirname,"../mails/activation-mail.ejs"),data)
 
     }catch(error:any){
         return next(new ErrorHandler(error.message,400))
