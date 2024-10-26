b28e27a11015cb91a70fdc9ba0fdc200dd7d23e8
NamNPH <80657434+nphnamm@users.noreply.github.com>
2024-10-26 17:58:34 +0700

    add logout

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -13,102 +13,102 @@ import { sendToken } from "../utils/jwt";
 
 
 // register user 
-interface IRegistrationBody{
+interface IRegistrationBody {
     name: string;
     email: string;
     password: string;
-    avatar?: string; 
+    avatar?: string;
 
 }
 
-export const registrationUser = CatchAsyncError(async(req:Request,res: Response, next:NextFunction)=>{
-    try{
-        const {name, email, password} = req.body;
-        const isEmailExist = await userModel.findOne({email});
-        if(isEmailExist){
-            return next(new ErrorHandler("Email already exist",400))
+export const registrationUser = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { name, email, password } = req.body;
+        const isEmailExist = await userModel.findOne({ email });
+        if (isEmailExist) {
+            return next(new ErrorHandler("Email already exist", 400))
         }
-        const user:IRegistrationBody={
+        const user: IRegistrationBody = {
             name,
-            email, 
+            email,
             password
         }
         // jwt create code activation
-        const activationToken= createActivationToken(user); 
-    
+        const activationToken = createActivationToken(user);
+
         const activationCode = activationToken.activationCode;
         const data = {
-            user:{
-                name:user.name,
+            user: {
+                name: user.name,
 
             },
             activationCode
         }
-        const html = await ejs.renderFile(path.join(__dirname,"../mails/activation-mail.ejs"),data)
-        try{
+        const html = await ejs.renderFile(path.join(__dirname, "../mails/activation-mail.ejs"), data)
+        try {
             sendMail({
-                email:user.email,
-                subject:"Activation account",
-                template:"activation-mail.ejs",
+                email: user.email,
+                subject: "Activation account",
+                template: "activation-mail.ejs",
                 data
             });
             res.status(201).json({
-                succues:true,
-                message:`Please check your email: ${user.email} to activate your account!`,
+                succues: true,
+                message: `Please check your email: ${user.email} to activate your account!`,
                 activationToken: activationToken.token
             })
-        }catch(error:any){
-            return next(new ErrorHandler(error.message,400))
-    
+        } catch (error: any) {
+            return next(new ErrorHandler(error.message, 400))
+
         }
 
-    }catch(error:any){
-        return next(new ErrorHandler(error.message,400))
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400))
     }
 });
-interface IActivationToken{
+interface IActivationToken {
     token: string;
     activationCode: string;
 }
-export const createActivationToken = (user:any): IActivationToken=>{
+export const createActivationToken = (user: any): IActivationToken => {
     const activationCode = Math.floor(1000 + Math.random() * 9000).toString();
     const token = jwt.sign({
         user,
         activationCode,
-    },process.env.ACTIVATION_SECRET as Secret
-    ,{
-        expiresIn:"5m"
-    });
-    return {token,activationCode}
+    }, process.env.ACTIVATION_SECRET as Secret
+        , {
+            expiresIn: "5m"
+        });
+    return { token, activationCode }
 }
 
 
 // activate   user
-interface IActivationRequest{
+interface IActivationRequest {
     activation_token: string;
     activation_code: string
 }
 
-export const activateUser = CatchAsyncError(async(req:Request,res: Response, next:NextFunction)=>{
-    try{
-        const {activation_token, activation_code} = req.body as IActivationRequest;
+export const activateUser = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { activation_token, activation_code } = req.body as IActivationRequest;
         // using jwt to verify and get activation code 
-        const newUser : {user: IUser; activationCode:string} = jwt.verify(
+        const newUser: { user: IUser; activationCode: string } = jwt.verify(
             activation_token,
             process.env.ACTIVATION_SECRET as string
-        ) as {user: IUser; activationCode:string};
+        ) as { user: IUser; activationCode: string };
 
-        if(newUser.activationCode !== activation_code){
-            return next(new ErrorHandler("Invalid activation code",400));
+        if (newUser.activationCode !== activation_code) {
+            return next(new ErrorHandler("Invalid activation code", 400));
 
 
         }
-        const {name,email,password} = newUser.user;
-        
-        const existUser = await userModel.findOne({email});
+        const { name, email, password } = newUser.user;
 
-        if(existUser){
-            return next(new ErrorHandler("User already exist",400));
+        const existUser = await userModel.findOne({ email });
+
+        if (existUser) {
+            return next(new ErrorHandler("User already exist", 400));
 
         }
         const user = await userModel.create({
@@ -118,63 +118,76 @@ export const activateUser = CatchAsyncError(async(req:Request,res: Response, nex
         });
         res.status(201).json({
             user,
-            success:true
+            success: true
         })
 
 
-        
-    }catch(error:any){
-        return next(new ErrorHandler(error.message,400))
+
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400))
 
     }
 });
 
 
 // login user
-interface ILoginRequest{
-    email:string;
-    password:string
+interface ILoginRequest {
+    email: string;
+    password: string
 
 }
 
 
-export const loginUser = CatchAsyncError(async(req:Request,res: Response, next:NextFunction)=>{
-    try{
-        const {email,password} = req.body as ILoginRequest;
+export const loginUser = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { email, password } = req.body as ILoginRequest;
 
-        if(!email || !password){
-            return next(new ErrorHandler("Please provide email and password",400));
+        if (!email || !password) {
+            return next(new ErrorHandler("Please provide email and password", 400));
         }
 
-        const user = await userModel.findOne({email}).select("+password");
-        
-        if(!user){
-            return next(new ErrorHandler("Invalid email or password",400));
+        const user = await userModel.findOne({ email }).select("+password");
+
+        if (!user) {
+            return next(new ErrorHandler("Invalid email or password", 400));
         };
 
         const isPasswordMatch = await user.comparePassword(password);
-        if(!isPasswordMatch){
-            return next(new ErrorHandler("Invalid email or password",400));
+        if (!isPasswordMatch) {
+            return next(new ErrorHandler("Invalid email or password", 400));
         }
-        sendToken(user,200,res);
+        sendToken(user, 200, res);
 
-    }catch(error:any){
-        return next(new ErrorHandler(error.message,400))
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400))
+
+    }
+});
+export const logoutUser = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        res.cookie("access_token", "", { maxAge: 1 });
+        res.cookie("refresh_token", "", { maxAge: 1 });
+        res.status(200).json({
+            success: true,
+            message: "Logged out successfully"
+        })
+
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400))
 
     }
 });
 
 
+export const template = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
 
-export const template = CatchAsyncError(async(req:Request,res: Response, next:NextFunction)=>{
-    try{
 
 
-        
 
-        
-    }catch(error:any){
-        return next(new ErrorHandler(error.message,400))
+
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400))
 
     }
 });
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,10 +1,11 @@
-import { activateUser, loginUser, registrationUser } from '../controllers/user.controller';
+import { activateUser, loginUser, logoutUser, registrationUser } from '../controllers/user.controller';
 import express from 'express';
 const router = express.Router();
 
 router.post('/registration', registrationUser);
 router.post('/activate-user', activateUser);
 router.post('/login-user', loginUser);
+router.post('/logout', logoutUser);
 
 
 
