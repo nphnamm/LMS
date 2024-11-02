c29618f425b99aa2d890d92173387c4b2d8d7379
NamNPH <80657434+nphnamm@users.noreply.github.com>
2024-11-02 23:02:11 +0700

    add animation shake and verification modal

--- client/app/components/Auth/Verification.tsx
+++ client/app/components/Auth/Verification.tsx
@@ -22,45 +22,84 @@ const Verification: FC<Props> = ({ setRoute }) => {
         useRef<HTMLInputElement>(null),
 
     ];
-    const [verifyNumber,setVerifyNumber] = useState<VerifyNumber>({
-        0:"",
-        1:"",
-        2:"",
-        3:""
+    const [verifyNumber, setVerifyNumber] = useState<VerifyNumber>({
+        0: "",
+        1: "",
+        2: "",
+        3: ""
     })
     const verificationHandler = async () => {
         console.log('test');
+        setInValidError(true); 
     };
-    const handleInputChange = (index:number,value:string)=>{
+    const handleInputChange = (index: number, value: string) => {
         setInValidError(false);
-        const newVerifyNumber = {...verifyNumber,[index]:value};
+        const newVerifyNumber = { ...verifyNumber, [index]: value };
         setVerifyNumber(newVerifyNumber);
-        if(value === "" && index >0){
-            inputRefs[index-1].current?.focus();
-        }else if(value.length === 1 && index < 3){
-            inputRefs[index+1].current?.focus();
+        if (value === "" && index > 0) {
+            inputRefs[index - 1].current?.focus();
+        } else if (value.length === 1 && index < 3) {
+            inputRefs[index + 1].current?.focus();
         };
-    } 
+    }
     return (
 
         <div>
             <h1 className={`${styles.title}`}>
                 Verify Your Account
             </h1>
-            <br/>
+            <br />
             <div className='w-full flex items-center justify-center mt-2'>
-                 <div className='w-[80px] h-[80px] rounded-full bg-[#497DF2] flex items-center justify-center'>
-                    <VscWorkspaceTrusted size={40}/>
-
-                 </div>
-      
-            </div>
-
-            <br/>
-                <br/>
-                <div className='1100px:w-[70%] m-auto flex items-center justify-around'>
+                <div className='w-[80px] h-[80px] rounded-full bg-[#497DF2] flex items-center justify-center'>
+                    <VscWorkspaceTrusted size={40} />
 
                 </div>
+
+            </div>
+
+            <br />
+            <br />
+            <div className='m-auto flex items-center justify-around'>
+                {Object.keys(verifyNumber).map((key,index)=>(
+                    <input
+                    type='text'
+                    key={key}
+                    ref={inputRefs[index]}
+                    className={`w-[65px] h-[65px] bg-transparent border-[3px] rounded-[10px] flex items-center text-black dark:text-white justify-center text-[18px] font-Poppins outline-none text-center ${
+                        inValidError ? "shake border-red-500" : "dark:border-white border-[#0000004a]"    
+                    }`}
+                    placeholder=''
+                    maxLength={1}
+                    value={verifyNumber[key as keyof VerifyNumber]}
+                    onChange={(e) => handleInputChange(index, e.target.value)}
+                    
+                    />
+                ))}
+            </div>
+            <br />
+            <br />
+            <div className='w-full flex justify-center'>
+                <button
+                className={`${styles.button}`}
+                onClick={verificationHandler}
+                >
+                    Verify OTP
+
+                </button>
+
+            </div>
+            <br/>
+            <h5 className='text-center pt-4 font-Poppins text-[14px] text-black dark:text-white'>
+                Go back to sign in?{" "}
+                <span 
+                className='text-[#2190ff] pl-1 cursor-pointer'
+                onClick={()=>setRoute("Login")}
+                >
+                    Sign In
+
+                </span>
+
+            </h5>
         </div>
     )
 }
