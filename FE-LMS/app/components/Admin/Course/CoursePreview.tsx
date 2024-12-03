5f3e31d11c92694bf824408d9d75b73d99f27bc4
NamNPH <80657434+nphnamm@users.noreply.github.com>
2024-12-03 14:55:06 +0700

    update the course creation section 1

--- FE-LMS/app/components/Admin/Course/CoursePreview.tsx
+++ FE-LMS/app/components/Admin/Course/CoursePreview.tsx
@@ -14,8 +14,16 @@ const CoursePreview: FC<Props> = ({
     active
 }) => {
   return (
-    <div className='w-[90%] m-auto py-5'>
+    <div className='w-[90%] m-auto py-5 mb-5'>
+        <div className='w-full relative'>
+            <div className='w-full mt-10'>
+                <CoursePlayer
+                    videoUrl={courseData?.demoUrl}
+                    title={courseData?.title}
+                />
+            </div>
 
+        </div>
     </div>
   )
 }
---
+++ FE-LMS/app/utils/CoursePlayer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {
+    videoUrl:string;
+    title:string;
+    description:string
+}
+
+const CoursePlayer = (props: Props) => {
+  return (
+    <div>CoursePlayer</div>
+  )
+}
+
+export default CoursePlayer
