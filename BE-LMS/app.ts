5bbd7a5a91f469e92e4f9e5396938014fca9d278
NamNPH <80657434+nphnamm@users.noreply.github.com>
2024-12-07 22:30:32 +0700

    add ai api

--- BE-LMS/app.ts
+++ BE-LMS/app.ts
@@ -15,6 +15,7 @@ import orderRouter from "./routes/order.route";
 import notificationRouter from "./routes/notification.route";
 import analyticsRouter from "./routes/analytics.route";
 import layoutRouter from "./routes/layout.route";
+import aiRouter from "./routes/ai.route";
 
 // body parser
 app.use(express.json({limit: "50mb"}));
@@ -41,6 +42,7 @@ app.use("/api/v1",studySessionRouter)
 app.use("/api/v1",notificationRouter)
 app.use("/api/v1",analyticsRouter)
 app.use("/api/v1",layoutRouter)
+app.use("/api/v1",aiRouter)
 
 
 
